346ec7ad5fd652b25f5fa9d4d6032fd06231703d
Kir1L73 <k.khasanov2002@gmail.com>
2022-06-28 12:44:51 +0400

    Начало

---
+++ src/blocks/modules/menu/menu.js
@@ -0,0 +1,19 @@
+// modules.define('menu', ['i-bem-dom'], function(provide, bemDom) {
+
+// provide(bemDom.declBlock(this.name, {
+//     onSetMod: {
+//         js: {
+//             inited: function() {
+                
+//             }
+//         }
+//     }
+// }));
+
+// });
+
+// $(".menu li ul").hide();
+// $(".menu li:has('.submenu')").hover(
+//   function(){
+//   $(".menu li ul").stop().fadeToggle(300);}
+// );
