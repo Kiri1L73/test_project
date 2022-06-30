875f290f8eb85d5b16061ea8ceef3cf8c2165a9a
Kir1L73 <k.khasanov2002@gmail.com>
2022-07-01 03:36:19 +0400

    все страницы+меню(кнопка вызова меню не работает)

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -17,3 +17,22 @@
 //   function(){
 //   $(".menu li ul").stop().fadeToggle(300);}
 // );
+
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.img_menu')) {
+
+//     var dropdowns = document.getElementsByClassName("menu_list");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
